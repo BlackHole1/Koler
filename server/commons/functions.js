29ef0ebb2b28318b593a6a315735680c89058d09
BlackHole1 <158blackhole@gmail.com>
2017-10-31 23:30:32 +0800

    使用了all-equal库来进行判断

--- server/commons/functions.js
+++ server/commons/functions.js
@@ -20,15 +20,6 @@ const fun = {
       return md5Text
     }
     return this.md5(arguments[0])
-  },
-  allEqual (...arrays) {
-    const result = []
-    for (let i = 0; i < arrays.length; i++) {
-      if (!this.isArray(arrays[i])) throw new Error(`arguments must be a Array`)
-      if (arrays[i].length < 1) throw new Error(`The number of array values must be greater than one`)
-      result.push(!arrays[i].some((value, index) => value !== arrays[i][0]))
-    }
-    return result.every((v) => v === true)
   }
 }
 
