abeb118efa3a22adc1fbc4ecb51b45ec9c215240
BlackHole1 <158blackhole@gmail.com>
2017-11-09 23:14:25 +0800

    解决token认证逻辑出现的bug

--- server/lib/server.js
+++ server/lib/server.js
@@ -28,23 +28,24 @@ server.use(function (req, res, next) {
     state: false,
     category: 'jwt'
   }
-  if (authorization === '' || authorization.indexOf(' ') === -1) { // 如果Authorization为空、不规范
+  if (authorization === '' || authorization === undefined || authorization.indexOf(' ') === -1) { // 如果Authorization为空、不规范
     jwtState.data = `no token detected in http header 'Authorization', 没有检测到头部信息里有Authorization字段`
-  }
-  const token = authorization.split(' ')[1]
-  /* eslint-disable */  // 此处可能存在eslint的bug，导致eslint报错，需要临时关闭下
-  jwt.verify(token, constant.jwt.secret, {  // 检测token是否符合规范
-    algorithms: [constant.jwt.algorithm]
-  }, (err,decoded) => {
-    if (err) {
-      if ('TokenExpiredError' === err.name) {
-        jwtState.data = 'the token is expired, 令牌过期'
+  } else {
+    const token = authorization.split(' ')[1]
+    /* eslint-disable */  // 此处可能存在eslint的bug，导致eslint报错，需要临时关闭下
+    jwt.verify(token, constant.jwt.secret, {  // 检测token是否符合规范
+      algorithms: [constant.jwt.algorithm]
+    }, (err,decoded) => {
+      if (err) {
+        if ('TokenExpiredError' === err.name) {
+          jwtState.data = 'the token is expired, 令牌过期'
+        }
+        jwtState.data = 'invalid token, token无效'
+      } else {
+        jwtState.state = true
       }
-      jwtState.data = 'invalid token, token无效'
-    } else {
-      jwtState.state = true
-    }
-  })
+    })
+  }
   /* esint-enable */
   if (jwtState.state) {
     return next()
