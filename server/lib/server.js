acf2f192303fc0f621de0dc89a472309a61c481c
BlackHole1 <158blackhole@gmail.com>
2017-11-13 22:33:16 +0800

    优化jwt验证的代码 1. 把jwt验证代码提取到公共方法里，方便后面其他功能调用

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,7 +1,7 @@
 const restify = require('restify')
-const jwt = require('jsonwebtoken')
 const corsMiddleware = require('restify-cors-middleware')
-const constant = require('../../src/commons/configConstant')
+const config = require('../../src/commons/configConstant')
+const common = require('../lib/common')
 
 const server = restify.createServer({
   name: 'Koler'
@@ -9,7 +9,7 @@ const server = restify.createServer({
 
 const cors = corsMiddleware({ // 解决跨域问题
   preflightMaxAge: 25,
-  origins: [constant.clientAddress],
+  origins: [config.clientAddress],
   allowHeaders: ['Authorization']
 })
 
@@ -23,30 +23,8 @@ server.use(function (req, res, next) {  // 请求hook，每次请求查看是否
   if (req.path() === '/Api/sign') { // 跳过注册页面
     return next()
   }
-  const authorization = req.header('Authorization')
-  let jwtState = {
-    state: false,
-    category: 'jwt'
-  }
-  if (authorization === '' || authorization === undefined || authorization.indexOf(' ') === -1) { // 如果Authorization为空、不规范
-    jwtState.data = `no token detected in http header 'Authorization', 没有检测到头部信息里有Authorization字段`
-  } else {
-    const token = authorization.split(' ')[1]
-    /* eslint-disable */  // 此处可能存在eslint的bug，导致eslint报错，需要临时关闭下
-    jwt.verify(token, constant.jwt.secret, {  // 检测token是否符合规范
-      algorithms: [constant.jwt.algorithm]
-    }, (err,decoded) => {
-      if (err) {
-        if ('TokenExpiredError' === err.name) {
-          jwtState.data = 'the token is expired, 令牌过期'
-        }
-        jwtState.data = 'invalid token, token无效'
-      } else {
-        jwtState.state = true
-      }
-    })
-  }
-  /* esint-enable */
+  const jwtState = common.jwt(req.header('Authorization'))
+
   if (jwtState.state) {
     return next()
   } else {
