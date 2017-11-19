eccd2302c4cc950029365a2c8f6250badcb4e163
BlackHole1 <158blackhole@gmail.com>
2017-11-19 17:48:28 +0800

    添加题库的Schema

--- server/model/schema/problemsWarehouse.js
+++ server/model/schema/problemsWarehouse.js
@@ -1,7 +1,25 @@
 const dateFormat = require('dateformat')
 module.exports = {
-  name: String,
-  upper: String,
-  upper_name: String,
-  created_date: dateFormat(new Date(), `yyyy-mm-dd HH:MM:ss`)
+  user: String, // 所属哪个用户
+  name: String, // 所属哪个题库
+  practiceNumber: Number, // 练习的次数
+  average: Number,  // 平均分
+  details: [  // 题目详情
+    {
+      name: String, // 题目标题
+      content: String,  // 题目内容
+      answer: String, // 答案
+      note: String, // 备注
+      category: Array,  // 所属类别
+      score: Number, // 分值
+      created_date: { // 题目创建时间
+        type: String,
+        default: dateFormat(new Date(), `yyyy-mm-dd HH:MM:ss`)
+      }
+    }
+  ],
+  created_date: { // 题库创建时间
+    type: String,
+    default: dateFormat(new Date(), `yyyy-mm-dd HH:MM:ss`)
+  }
 }
