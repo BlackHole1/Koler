0f9c4456c7a9ad18696501c21aecbf7d74abbf91
BlackHole1 <158blackhole@gmail.com>
2017-11-21 19:05:33 +0800

    创建题库-初始化后端

--- server/controller/problemsWarehouse.js
+++ server/controller/problemsWarehouse.js
@@ -1,4 +1,7 @@
 const M = require('../model')
+const empty = require('is-empty')
+const common = require('../lib/common')
+
 const resource = {
   getInfo: (req, res, next) => {
     let result = {}
@@ -21,6 +24,33 @@ const resource = {
           PWData[el.name] = el
         })
         result = PWData
+      }
+      res.send(result)
+    })
+  },
+  addPW: (req, res, next) => {
+    const result = {
+      state: false,
+      data: ''
+    }
+    if (empty(req.bofy.name)) {
+      result.data = '请输入要创建的题库名称'
+    }
+    const name = req.bofy.name
+    let PWMolde = M('problemsWarehouse')
+    const PWEntity = new PWMolde({
+      user: common.jwt(req.header('Authorization')).data.data.name,
+      name: name,
+      practiceNumber: 0,
+      average: 0,
+      details: []
+    })
+    PWEntity.save(function (err) {
+      if (err) {
+        result.data = err
+      } else {
+        result.state = true
+        result.data = '创建题库成功'
       }
       res.send(result)
     })
