ced489454ea6115e5579732c3e6c3132c6f620f3
BlackHole1 <158blackhole@gmail.com>
2017-11-24 14:10:14 +0800

    优化main的显示
    1. 无论有没有题库，都把用户信息页面显示出来

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,25 +1,18 @@
 <template>
   <div class="main">
     <v-header></v-header>
-    <!-- 判断数据库里是否有数据 -->
-    <!-- 如果没有数据 -->
-    <el-row :gutter="20" class="row" v-if='!getProblemsWarehouseInfo'> 
-      <el-col :span="12" :offset="4">
+    <el-row type="flex" class="row" justify="center">
+      <!-- 判断数据库里是否有数据 -->
+      <!-- 如果没有数据 -->
+      <el-col :span="12" v-if='!getProblemsWarehouseInfo'>
         <v-jumbotron>
           <h1>Sorry</h1>
           <p>您还没有创建任何的题目</p>
           <el-button type="primary" size="large" @click="toggleDialog('create')" :loading="loading">点我创建</el-button>
         </v-jumbotron>
       </el-col>
-      <el-col :span="1">
-      </el-col>
-      <el-col :span="5">
-        <v-nav-right ></v-nav-right>
-      </el-col>
-    </el-row>
-    <!-- 如果数据里有数据 -->
-    <el-row type="flex" class="row" justify="center" v-else>
-      <el-col :span="12">
+      <!-- 如果数据里有数据 -->
+      <el-col :span="12" v-else>
         <!-- 判断是否已经进入到题库里 -->
         <!-- 如果没有，则显示题库标题 -->
         <v-jumbotron v-if="!this.$route.params.name">
@@ -33,6 +26,7 @@
         <!-- 如果有，则说明已经进入到router里，则显示router对应的组件 -->
         <router-view v-else></router-view>
       </el-col>
+      <!-- 无论如何都显示右侧用户信息 -->
       <el-col :span="1">
       </el-col>
       <el-col :span="5">
