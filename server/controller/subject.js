ea64a075efd719e02867dc14c406e581e73a1a06
BlackHole1 <158blackhole@gmail.com>
2018-01-05 17:01:54 +0800

    "增加题目"后端代码已由callback转换成promise

--- server/controller/subject.js
+++ server/controller/subject.js
@@ -3,46 +3,47 @@ const empty = require('is-empty')
 
 const resource = {
   add: (req, res, next) => {
-    const result = {
-      state: false,
-      data: ''
-    }
     if (empty(req.body.name) || empty(req.body.title) || empty(req.body.content) || empty(req.body.score)) {
-      result.data = '值不能为空'
-      res.send(result)
-    } else {
-      const {name, title, content, score, tags, note, answer} = req.body
-      const email = req.$getInfo.email
-      let PWMolde = M('problemsWarehouse')
-      PWMolde.findByEmailAndName(email, name, function (err, data) {
-        if (err || data.length !== 1) {
-          result.data = !(err) ? '找不到此题库' : err
-          res.send(result)
-        } else {
-          PWMolde.update({
-            email: email,
-            name: name
-          }, {
-            $push: {
-              details: {
-                name: title,
-                content: content,
-                answer: answer,
-                note: note,
-                category: tags,
-                score: score
-              }
-            }
-          }, {
-            enable: false
-          }, function (err) {
-            result.state = !(err)
-            result.data = (err) ? '添加题目失败' : '添加题目成功'
-            res.send(result)
-          })
-        }
+      return res.send({
+        state: false,
+        data: '值不能为空'
       })
     }
+    const {name, title, content, score, tags, note, answer} = req.body
+    const email = req.$getInfo.email
+    let PWMolde = M('problemsWarehouse')
+    PWMolde.findByEmailAndName1(email, name)
+      .catch(() => Promise.reject('连接数据库出错'))
+      .then(data => {
+        if (data.length !== 1) {
+          return Promise.reject('找不到此题库')
+        }
+        return PWMolde.update({
+          email: email,
+          name: name
+        }, {
+          $push: {
+            details: {
+              name: title,
+              content: content,
+              answer: answer,
+              note: note,
+              category: tags,
+              score: score
+            }
+          }
+        }, {
+          enable: false
+        })
+          .catch(() => Promise.reject('添加题目失败'))
+          .then(data => Promise.resolve('添加题目成功'))
+      })
+      .unified((state, data) => {
+        res.send({
+          state,
+          data
+        })
+      })
   },
   del: (req, res, next) => {
     const result = {
