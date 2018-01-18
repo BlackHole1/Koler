b182333bb8da190b7f73ba5a33a07516cd9de341
BlackHole1 <158blackhole@gmail.com>
2018-01-18 21:06:05 +0800

    完善添加用户的前端展示

--- src/components/AddUser/AddUser.vue
+++ src/components/AddUser/AddUser.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="add-user">
-    添加{{subName}}
+    <el-form label-width="80px" :model="userInfoSet" :rules="rulesUserInfo" label-position="left">
+      <el-form-item label="设置姓名" prop="name">
+        <el-input v-model="userInfoSet.name" size="medium"></el-input>
+      </el-form-item>
+      <el-form-item label="设置邮箱" prop="email">
+        <el-input v-model="userInfoSet.email" size="medium"></el-input>
+      </el-form-item>
+      <el-form-item label="设置密码" prop="password">
+        <el-input v-model="userInfoSet.password" size="medium"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" size="small">确认添加{{subName}}</el-button>
   </div>
 </template>
 
@@ -12,10 +23,40 @@ export default {
     subName () {
       return common.subName(this.$store.getters.getUser.type)
     }
+  },
+  data: () => {
+    return {
+      userInfoSet: {
+        name: '',
+        email: '',
+        password: ''
+      },
+      rulesUserInfo: {
+        name: [
+          {required: true, message: '姓名不能为空', trigger: 'blur'}
+        ],
+        email: [
+          {required: true, message: '邮箱不能为空', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '密码不能为空', trigger: 'blur'}
+        ]
+      }
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang='less'>
+form {
+    label, .el-form-item__content {
+      float: left;
+    }
+    .el-form-item__content {
+      input {
+        width: 200%;
+      }
+      margin-left: 0 !important;
+    }
+  }
 </style>
