30bcbdc9a55771057595ddb937bd5e982687364a
BlackHole1 <158blackhole@gmail.com>
2018-01-19 13:56:35 +0800

    删除vuex多余的文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
-import * as getters from './getters'
 import login from './modules/login'
 import problemsWarehouse from './modules/problemsWarehouse'
 import navRight from './modules/navRight'
@@ -9,8 +7,6 @@ import navRight from './modules/navRight'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  actions,
-  getters,
   modules: {
     login,
     problemsWarehouse,
