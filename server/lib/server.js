163496694ccea7683808b842dd3d01427a34c3c5
BlackHole1 <158blackhole@gmail.com>
2018-01-23 19:39:09 +0800

    完成后端针对请求Hook的优化

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,8 +1,7 @@
 const restify = require('restify')
 const corsMiddleware = require('restify-cors-middleware')
 const config = require('../../common/config')
-const common = require('../lib/common')
-const M = require('../model')
+const middlewareHook = require('../middleware/hook')
 
 const server = restify.createServer({
   name: 'Koler'
@@ -20,32 +19,6 @@ server.use(cors.actual)
 server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
 
-server.use(function (req, res, next) {  // 请求hook，每次请求查看是否有登录
-  if (req.path() === '/Api/sign') { // 跳过注册页面
-    return next()
-  }
-  const jwtState = common.jwt(req.header('Authorization'))
-  if (jwtState.state) {
-    req.$getInfo = jwtState.data.data
-    const UserModel = M('user')
-    UserModel.findByEmail(req.$getInfo.email)
-      .catch(() => Promise.reject('连接数据库出错'))
-      .then(data => Promise.resolve(data))
-      .unified((state, data) => {
-        if (state) {
-          req.$currentUserInfo = data
-          return next()
-        } else {
-          return res.send({
-            state,
-            data
-          })
-        }
-      })
-  } else {
-    res.contentType = 'json'
-    return res.send(jwtState)
-  }
-})
+server.use(middlewareHook())
 
 module.exports = server
