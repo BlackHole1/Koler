df40b6042158b051c174982e7a80f7ed65ee2c7a
BlackHole1 <158blackhole@gmail.com>
2018-01-29 08:35:14 +0800

    优化后端中间件的验证hook文件的排版

--- server/middleware/validate/index.js
+++ server/middleware/validate/index.js
@@ -1,44 +1,47 @@
 const empty = require('is-empty')
 
-const sign = {
-  post: cb => {
+const problemsWarehouse = {
+  add: cb => {
     return (req, res, next) => {
-      if (empty(req.body.email) || empty(req.body.pass)) {
-        res.send({
+      if (empty(req.body.name)) {
+        return res.send({
           state: false,
-          data: '账号或密码不能为空'
+          data: '请输入要创建的题库名称'
         })
-        return false
+      }
+      cb(req, res, next)
+    }
+  },
+  update: cb => {
+    return (req, res, next) => {
+      if (empty(req.body.name) || empty(req.body.changeName)) {
+        return res.send({
+          state: false,
+          data: '值不能为空'
+        })
+      }
+    }
+  },
+  del: cb => {
+    return (req, res, next) => {
+      if (empty(req.query.name)) {
+        return res.send({
+          state: false,
+          data: '请确保您要删除的题目是否存在'
+        })
       }
       cb(req, res, next)
     }
   }
 }
 
-const users = {
-  getList: cb => {
-    return (req, res, next) => {
-      if (req.$currentUserInfo.type === 'Student') {
-        return res.send({
-          state: false,
-          data: '很抱歉，你没用权限进行列举你的下属'
-        })
-      }
-      cb(req, res, next)
-    }
-  },
+const subject = {
   add: cb => {
     return (req, res, next) => {
-      if (empty(req.body.name) || empty(req.body.email) || empty(req.body.password)) {
+      if (empty(req.body.name) || empty(req.body.title) || empty(req.body.content) || empty(req.body.score)) {
         return res.send({
           state: false,
-          data: '姓名、邮箱、密码等值不能为空，请检查后重新提交'
-        })
-      }
-      if (req.$currentUserInfo.type === 'Student') {
-        return res.send({
-          state: false,
-          data: '很抱歉，你没用权限进行添加用户'
+          data: '值不能为空'
         })
       }
       cb(req, res, next)
@@ -46,16 +49,10 @@ const users = {
   },
   del: cb => {
     return (req, res, next) => {
-      if (empty(req.query.id)) {
+      if (empty(req.query.name) || empty(req.query.id)) {
         return res.send({
           state: false,
-          data: 'id的值不能为空'
-        })
-      }
-      if (req.$currentUserInfo.type === 'Student') {
-        return res.send({
-          state: false,
-          data: '你没用权限进行删除用户'
+          data: '请确认传入了值'
         })
       }
       cb(req, res, next)
@@ -110,13 +107,30 @@ const user = {
   }
 }
 
-const subject = {
-  add: cb => {
+const users = {
+  getList: cb => {
     return (req, res, next) => {
-      if (empty(req.body.name) || empty(req.body.title) || empty(req.body.content) || empty(req.body.score)) {
+      if (req.$currentUserInfo.type === 'Student') {
         return res.send({
           state: false,
-          data: '值不能为空'
+          data: '很抱歉，你没用权限进行列举你的下属'
+        })
+      }
+      cb(req, res, next)
+    }
+  },
+  add: cb => {
+    return (req, res, next) => {
+      if (empty(req.body.name) || empty(req.body.email) || empty(req.body.password)) {
+        return res.send({
+          state: false,
+          data: '姓名、邮箱、密码等值不能为空，请检查后重新提交'
+        })
+      }
+      if (req.$currentUserInfo.type === 'Student') {
+        return res.send({
+          state: false,
+          data: '很抱歉，你没用权限进行添加用户'
         })
       }
       cb(req, res, next)
@@ -124,10 +138,16 @@ const subject = {
   },
   del: cb => {
     return (req, res, next) => {
-      if (empty(req.query.name) || empty(req.query.id)) {
+      if (empty(req.query.id)) {
         return res.send({
           state: false,
-          data: '请确认传入了值'
+          data: 'id的值不能为空'
+        })
+      }
+      if (req.$currentUserInfo.type === 'Student') {
+        return res.send({
+          state: false,
+          data: '你没用权限进行删除用户'
         })
       }
       cb(req, res, next)
@@ -135,35 +155,15 @@ const subject = {
   }
 }
 
-const problemsWarehouse = {
-  add: cb => {
+const sign = {
+  post: cb => {
     return (req, res, next) => {
-      if (empty(req.body.name)) {
-        return res.send({
+      if (empty(req.body.email) || empty(req.body.pass)) {
+        res.send({
           state: false,
-          data: '请输入要创建的题库名称'
+          data: '账号或密码不能为空'
         })
-      }
-      cb(req, res, next)
-    }
-  },
-  update: cb => {
-    return (req, res, next) => {
-      if (empty(req.body.name) || empty(req.body.changeName)) {
-        return res.send({
-          state: false,
-          data: '值不能为空'
-        })
-      }
-    }
-  },
-  del: cb => {
-    return (req, res, next) => {
-      if (empty(req.query.name)) {
-        return res.send({
-          state: false,
-          data: '请确保您要删除的题目是否存在'
-        })
+        return false
       }
       cb(req, res, next)
     }
