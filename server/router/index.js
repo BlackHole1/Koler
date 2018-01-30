42e242047ca2be3b31df89364283f5b21037a6b1
BlackHole1 <158blackhole@gmail.com>
2018-01-30 12:16:08 +0800

    对路由代码添加注释

--- server/router/index.js
+++ server/router/index.js
@@ -7,13 +7,13 @@ const recursion = (obj, path = '') => {  // 针对routes.js的路由配置挂载
   return () => {
     for (let key in obj) {
       const val = obj[key]
-      if (common.isObject(val)) { // 如果还有下一层路由配置
+      if (common.isObject(val)) { // 如果是对象，说明还有下一层路由配置
         recursion(val, `${path}/${key}`)()
-      } else if (common.isArray(val)) {
+      } else if (common.isArray(val)) { // 如果是数组，则是验证与核心代码分离的
         let validateFun = val[0]
         let mainFun = val[1]
         server[key](path, validateFun(mainFun))
-      } else {
+      } else {  // 如果不是对象、数组。说明是控制层函数
         server[key](path, val)
       }
     }
