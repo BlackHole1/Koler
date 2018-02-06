b41e7294292486d63985845f3fa3abed749ea22a
BlackHole1 <158blackhole@gmail.com>
2018-02-07 00:19:32 +0800

    优化删除日志的输出

--- build/deletePm2Logs.js
+++ build/deletePm2Logs.js
@@ -1,7 +1,5 @@
 let del = require('delete')
 
-console.log('开始删除pm2的logs文件')
-
 del(['./logs/*.log'], {force: true}, function(err, deleted) {
   if (err) {
     console.log('无法删除pm2的logs文件，请手动删除/logs目录下的日志文件')
