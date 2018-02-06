b78afc06e52212d1544ef46d2415c12a7f44f556
BlackHole1 <158blackhole@gmail.com>
2018-02-07 00:42:09 +0800

    完善前端考试系统的显示

--- src/components/Exam/Exam.vue
+++ src/components/Exam/Exam.vue
@@ -2,22 +2,34 @@
   <div class="exam">
     <v-header></v-header>
     <el-row type="flex" class="row" justify="center">
-      考试系统
+      <el-col :span="14">
+        <v-jumbotron>
+          <p>
+            在下面选择你要考试的模式
+          </p>
+          <el-button type="primary" icon="el-icon-edit">自己出卷考试</el-button>
+          <el-button type="primary" icon="el-icon-edit-outline">老师发卷考试</el-button>
+        </v-jumbotron>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 import Header from '~/Header'
+import Jumbotron from '~/Jumbotron'
 export default {
   name: 'Exam',
   components: {
-    'v-header': Header
+    'v-header': Header,
+    'v-jumbotron': Jumbotron
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+  .row {
+    margin-top: 20px;
+  }
 </style>
 
