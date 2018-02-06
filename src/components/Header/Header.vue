1fd71e14b9805d6849512648b38dc850c52993e6
BlackHole1 <158blackhole@gmail.com>
2018-02-07 00:31:39 +0800

    添加考试系统只对学生有效的功能

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" menu-trigger="click" :router="true">
       <el-menu-item index="/" class="nav-left">我的题库</el-menu-item>
-      <el-menu-item index="/Exam">在线考试</el-menu-item>
+      <el-menu-item index="/Exam" v-if="examShow">在线考试</el-menu-item>
       <el-menu-item index="/UserManage" v-if="userManageShow">用户管理</el-menu-item>
       <el-submenu index="2" class="nav-right">
         <template slot="title">{{getUser.name}}</template>
@@ -41,6 +41,10 @@ export default {
     userManageShow () {
       const currentUserlevel = this.getUser.type
       return (currentUserlevel && currentUserlevel !== 'Student')
+    },
+    examShow () {
+      const currentUserlevel = this.getUser.type
+      return (currentUserlevel && currentUserlevel === 'Student')
     }
   }
 }
