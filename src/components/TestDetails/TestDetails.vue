91640d6863bcd0ebe8c1f1d80ae6cc6dbd000230
BlackHole1 <158blackhole@gmail.com>
2018-03-25 17:12:57 +0800

    完成前端试题删除的答题框架

--- src/components/TestDetails/TestDetails.vue
+++ src/components/TestDetails/TestDetails.vue
@@ -232,6 +232,20 @@ export default {
           this.$refs[`${model}Simplemde`].simplemde.togglePreview()
         }
       })
+    },
+    deleteTest (id) {
+      this.$confirm('此操作将永久删除该试题, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+      .then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      })
+      .catch(() => {})
     }
   },
   components: {
