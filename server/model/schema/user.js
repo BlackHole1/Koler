7d7e199dc288189ed7e670fdd5c5995bb23b25c1
BlackHole1 <158blackhole@gmail.com>
2018-05-26 19:13:16 +0800

    为user的schema添加说明注释

--- server/model/schema/user.js
+++ server/model/schema/user.js
@@ -2,19 +2,19 @@ const mongoose = require('../../lib/mongoose')
 const dateFormat = require('dateformat')
 
 const user = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  type: String,
-  upper: String,
-  upper_email: String,
-  upper_name: String,
-  under: String,
-  avatar_url: {
+  name: String, // 用户姓名
+  email: String,  // 用户邮箱
+  password: String, // 用户密码
+  type: String, // 用户类别
+  upper: String,  // 用户上级类别
+  upper_email: String,  // 用户上级邮箱
+  upper_name: String, // 用户上级名称
+  under: String,  // 下级用户类别
+  avatar_url: { // 默认头像地址
     type: String,
     default: '/static/userHeader/defaultUserHeader.png'
   },
-  created_date: {
+  created_date: { // 用户创建时间
     type: String,
     default: dateFormat(new Date(), `yyyy-mm-dd HH:MM:ss`)
   }
